9d05d2b756bea60ab10be1e7355275cdd0d78edd
jyy <jyy@dhcp-v880-14-00569.1Xwireless.unc.edu>
2017-11-06 08:03:04 -0500

    Loading Screen

--- app/components/DataTable.jsx
+++ app/components/DataTable.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-// const apiBaseUrl = "http://localhost:3000/api/";
-const apiBaseUrl = "http://hidden-atoll-90934.herokuapp.com/api/";
+const apiBaseUrl = "http://localhost:3000/api/";
+// const apiBaseUrl = "http://hidden-atoll-90934.herokuapp.com/api/";
 import request from 'superagent';
 import axios from 'axios';
+import {BarChart} from 'react-easy-chart';
+
+
 
 
 class DataTable extends React.Component {
@@ -12,21 +15,36 @@ constructor(props) {
     super(props);
 
     this.state = {
+      start: "loading...",
       listingsData: [],
       listingsDataTemp: [],
-      searchValue: ""
+      searchValue: "",
+      sum: 0
     };
 
 }
 
-
 componentDidMount = () => {
+  //gets listing.csv data and stores that data in listingsDataTemp
   request.get(apiBaseUrl + 'getAllListings').set('API-Key', 'foobar').set('Accept', 'application/json').end((err, res) => {
       var data = res.body.results.data;
-      this.setState({listingsDataTemp: data})
+      //allows data to be parsed before loading components
+      setTimeout(function() { this.setState({listingsDataTemp: data, start: ""}); }.bind(this), 30000);
   })
-
+  var tempData = this.state.listingsDataTemp;
+  var BayviewPrice = 10, BayviewCount = 5;
+   tempData.map((list) => {
+    switch(list.neighbourhood){
+      case "Bayview":
+        var price = Number(list.price.replace(/[^0-9\.-]+/g,""))
+        BayviewPrice += price;
+        BayviewCount++;
+        break;
+    }
+  })
+  this.setState({ sum: BayviewPrice / BayviewCount })
 }
+
 handleSearch = (text) => {
 
       if (text === " ") {
@@ -80,42 +98,77 @@ renderResult = () =>{
 
 }
 
+getAverage = () => {
+
+}
+
+renderChart = () => {
+
+  // return <BarChart height={550} colorBars width={750} axes  axisLabels={{x: 'My x Axis', y: 'My y Axis'}}
+  //   data={[
+  //     {x: 'A', y: this.state.sum}
+  //   ]}
+  // />
+  return (
+    <div>
+      <h1>{this.state.sum}</h1>
+    </div>
+  )
+}
+renderData = () => {
+  return (
+    <div>
+      <div>
+        {this.renderChart()}
+      </div>
+
+    <div className = "makeSmaller">
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref="text" placeholder="Search Neighbourhoods"/>
+          <button className="button expanded custom">Search</button>
+        </form>
+        <table className = "hover unstriped">
+          <thead>
+            <tr></tr>
+            <tr>
+              <th>Id</th>
+              <th>Neighbourhood</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Price</th>
+              <th>Property Type</th>
+              <th>Room Type</th>
+              <th>Beds</th>
+              <th>Cleaning fee</th>
+              <th>Reviews per Month</th>
+              <th>Security Deposit</th>
+              <th>Review Scores Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderResult()}
+          </tbody>
+        </table>
+
+    </div>
+  </div>
+    </div>
+  )
+}
+renderStart = () =>{
+  return(
+    <div><h1>
+      LOADING... Please wait about 20 seconds
+    </h1></div>
+  )
+}
 render() {
-    return (
-      <div className = "makeSmaller">
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" ref="text" placeholder="Search Neighbourhoods"/>
-            <button className="button expanded custom">Search</button>
-          </form>
-          <table className = "hover unstriped">
-            <thead>
-              <tr></tr>
-              <tr>
-                <th>Id</th>
-                <th>Neighbourhood</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Price</th>
-                <th>Property Type</th>
-                <th>Room Type</th>
-                <th>Beds</th>
-                <th>Cleaning fee</th>
-                <th>Reviews per Month</th>
-                <th>Security Deposit</th>
-                <th>Review Scores Rating</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.renderResult()}
-            </tbody>
-          </table>
-
-      </div>
-
-      </div>
-    )
-
+  return(
+    <div>
+      {this.state.start === "loading..." ? this.renderStart() : this.renderData()}
+    </div>
+)
   }
 
 };
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ router.get('/getAllListings', function(req,res){
   Papa.parse(fs.createReadStream('./listings.csv'), {
        delimiter: ",",
        header: true,
-
+       dynamicTyping: true,
       //  complete: function(results) {
       //        res.send({"results": results});
       //  }
